35735982e2cbf8226f1a270e0e41a79595fe8d9c
manha1895 <manha1895@hotmail.com>
2021-07-21 15:53:32 -0700

    feat contactus

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="page-wrapper">
+      <div className="page-break-point">
+        <Component {...pageProps} />;
+      </div>
+    </div>
+  );
 }
 
 export default MyApp;
