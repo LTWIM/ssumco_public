93abeedbe085b8d02990002a9fdb439e4f69b9a5
manha1895 <manha1895@hotmail.com>
2021-07-21 11:26:21 -0700

    edit

---
+++ components/banner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from '../styles/module_stylesheets/banner.module.scss'
+export default function Banner () {
+    return(
+         <div>
+
+         </div>
+    )
+}
