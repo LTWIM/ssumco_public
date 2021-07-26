d5732f2f844dfe4a28f085adcc13e369f065d4bf
joy <joy109lee@gmail.com>
2021-07-26 10:18:35 -0700

    feat : teambanner

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,7 @@
 import React from "react";
 import styles from "../styles/About.module.scss";
-import {TopBanner, IconBanner, PhotoBanner} from '../components/index'
+import {TopBanner, IconBanner, PhotoBanner, TeamBanner} from '../components/index'
 export default function Home() {
-
   const cardInfo = [
     {
       title : "Security", 
@@ -29,6 +28,27 @@ export default function Home() {
       color : "#ff882e"
     }
   ]
+  const memberInfo = [
+    {
+      name: 'Joy Lee',
+      position: "Front end developer",
+      detail: "Donec pharetra nec diam in pharetra. Maecenas in metus dui. Vestibulum vestibulum lacinia mi. Vivamus massa dui",
+      photo:''
+    },
+    {
+      name: 'Paul Choi',
+      position: "CEO",
+      detail: "Nulla lacus ipsum, tincidunt in dui sit amet, fringilla vehicula tellus. Duis posuere gravida nibh a sollicitudin. Nulla pretium",
+      photo:''
+    },
+    {
+      name: 'Shawn Lee',
+      position: "Full stack developer",
+      detail: "Aenean dui arcu, porttitor vel orci id, euismod commodo sem. Aliquam rutrum ultricies orci",
+      photo:''
+    }
+  ]
+  
   return(
   <div className={styles.About}>
     <TopBanner 
@@ -48,6 +68,11 @@ export default function Home() {
     body="Maecenas est arcu, sagittis eget ante semper, ornare rhoncus tellus. In ac arcu at velit sollicitudin ultricies. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam eu euismod quam. Vivamus commodo hendrerit est, sed vestibulum quam mollis semper. Etiam tristique rutrum lectus a consequat. Praesent tristique cursus tellus, in ultricies"
     templates={350}
     websites={1.9}
+    />
+    <TeamBanner
+      header="Our Forward Thinkers"
+      body="Front end developer"
+      member = {memberInfo}
     />
   </div>
   ) 
