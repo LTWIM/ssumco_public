160928128f331634016baebec802654f9cc1c90d
joy <joy109lee@gmail.com>
2021-07-28 09:22:02 -0700

    feat : nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import styles from '../styles/module_stylesheets/Nav.module.scss'
 export default function Nav (props) {
+
+  const page = (name) => {
+    
+    return(
+      <div className={styles.Nav__pageCont}>
+        <div className={styles.Nav__page}>
+          {name}
+          <div className={styles.Nav__currentPage}/>
+        </div>
+      </div>
+    )
+  }
     return(
         <div className={styles.Nav}>
           <div className={styles.Nav__wrapper}>
@@ -13,53 +25,14 @@ export default function Nav (props) {
             
             <div className={styles.Nav__pageWrapper}>
               <div className={styles.Nav__pageSection}>
-                <div className={styles.Nav__pageCont}>
-                  <div className={styles.Nav__page}>
-                    Home
-                    <div className={styles.Nav__currentPage}/>
-                  </div>
-                </div>
-                
-                <div className={styles.Nav__pageCont}>
-                  <div className={styles.Nav__page}>
-                    About
-                    <div className={styles.Nav__currentPage}/>
-                  </div>
-                </div>
-                
-                <div className={styles.Nav__pageCont}>
-                  <div className={styles.Nav__page}>
-                    Prices
-                    <div className={styles.Nav__currentPage}/>
-                  </div>
-                </div>
-                
-                <div className={styles.Nav__pageCont}>
-                  <div className={styles.Nav__page}>
-                    Pages
-                    <div className={styles.Nav__currentPage}/>
-                  </div>
-                </div>
 
-                <div className={styles.Nav__pageCont}>
-                  <div className={styles.Nav__page}>
-                    Blog
-                    <div className={styles.Nav__currentPage}/>
-                  </div>
-                </div>
-
-                <div className={styles.Nav__pageCont}>
-                  <div className={styles.Nav__page}>
-                    Contact
-                    <div className={styles.Nav__currentPage}/>
-                  </div>
-                </div>
-
-                <div className={styles.Nav__pageCont}>
-                  <div className={styles.Nav__Facebook}>
-                    Facebook
-                  </div>
-                </div>
+                {page("Home")}
+                {page("About")}
+                {page("Prices")}
+                {page("Pages")}
+                {page("Blog")}
+                {page("Contact")}
+                {page("Facebook")}
 
                 <div className={styles.Nav__pageCont}>
                   <div className={styles.Nav__search}>
