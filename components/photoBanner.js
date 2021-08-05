be09a61c448b7b27fabdcd005a226ae4c7bf4371
joy <joy109lee@gmail.com>
2021-08-05 11:52:50 -0700

    edit : services photobanner

--- components/photoBanner.js
+++ components/photoBanner.js
@@ -74,6 +74,8 @@ export default function PhotoBanner(props) {
         return styles.PhotoBanner__cont__type4;
       case "7":
         return styles.PhotoBanner__cont__type1;
+      case "8":
+        return styles.PhotoBanner__cont__type1;
       default:
         break;
     }
@@ -135,6 +137,11 @@ export default function PhotoBanner(props) {
           );
         case "7":
           break;
+
+        case "8":
+          return(
+            <UseBackground type='3'/>
+          )
       default:
         break;
     }
@@ -243,6 +250,27 @@ export default function PhotoBanner(props) {
           </div>
         )
       case"7":
+        return(
+          <div className={`${styles.PhotoBanner__blockcont} ${styles.type4}`}>
+          <div className={styles.PhotoBanner__blockWrapper}>
+            {props.contents.map((block, index) => (
+              <CreateBlock
+                selected={blockSelected}
+                value={index}
+                blockref={getOrCreateRef(index)}
+                type={props.type}
+                onChange={handleChangeOnBlock}
+                title = {block.title}
+                body = {block.body}
+              />
+            ))}
+          </div>
+        {props.footer && <div className={`${styles.PhotoBanner__btn} ${styles.bg}`}>
+          {props.footer}
+        </div>}
+        </div>
+        )
+        case"8":
         return(
           <div className={`${styles.PhotoBanner__blockcont} ${styles.type4}`}>
           <div className={styles.PhotoBanner__blockWrapper}>
