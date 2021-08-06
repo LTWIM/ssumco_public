8abb043e843d1c50b0b81bda8f8909bfba7ce427
joy <joy109lee@gmail.com>
2021-08-06 15:44:47 -0700

    feat : Mobile Nav done

--- components/Nav.js
+++ components/Nav.js
@@ -1,26 +1,63 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from '../styles/module_stylesheets/Nav.module.scss'
-import { Facebook } from './svg'
+import { Facebook, Close } from './svg'
 export default function Nav (props) {
+
+  const [hamburgerClicked, setHamburherClicked] = useState(false)
 
   const page = (name) => {
     return(
-      <div className={styles.Desktop__Nav__pageCont}>
-        <div className={styles.Desktop__Nav__page}>
-          {name} <div className={styles.Desktop__Nav__currentPage}/>
+      <div className={styles.Nav__pageCont}>
+        <div className={styles.Nav__page}>
+          {name} 
+          <div className={styles.Nav__currentPage}/>
         </div>
       </div>
     )
   }
 
-  const logo = (name) => {
+  const logo = (name) => {  
     return(
-      <div className={styles.Desktop__Nav__logoCont}>
-        <div className={styles.Desktop__Nav__logo}>{name}</div>
+      <div className={styles.Nav__logoCont}>
+        <div className={styles.Nav__logo}>{name}</div>
       </div>
     )
   }
 
+  const hamburger = (width, height) => {
+    return(
+      <div className={styles.Hamburger_cont} style={{
+        width:`${width}`,
+        height:`${height}`}}
+        onClick={()=>{setHamburherClicked(true)}}
+      >
+        <div className={styles.Hamburger_bar} />
+        <div className={styles.Hamburger_bar_mid} />
+        <div className={styles.Hamburger_bar} />
+      </div>
+    )
+  }
+
+  const mobileMenu = () => {
+
+    return(
+      <div className={styles.mobileMenu} style={hamburgerClicked 
+        ?{left:'0px'}
+        :{left:'-400px'}}>
+        <div className={styles.mobileMenu__close} onClick={()=>{setHamburherClicked(false)}}>
+          {Close("30px")}
+        </div>
+        <div className={styles.mobileMenu__cont}>
+          {page("Home")}
+          {page("About")}
+          {page("Prices")}
+          {page("Pages")}
+          {page("Blog")}
+          {page("Contact")}
+        </div>  
+      </div>
+    )
+  }
 
   return(
     <div className={styles.Nav}>
@@ -42,8 +79,15 @@ export default function Nav (props) {
       </div>
 
       <div className={styles.Mobile__Nav}>
-        
+        {hamburger("34px", "32px")}
+        <div>
+        {logo("SSUMCO")}
+        </div>
+        <div className={styles.Mobile__Nav__Facebook}> 
+        {page(Facebook("36px"))}
+        </div>
       </div>
+      {mobileMenu()}
     </div>
   )
 }
