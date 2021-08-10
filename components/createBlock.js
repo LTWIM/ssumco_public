236b74aeb9dcc88aa72bef1a4779820cc78d150d
manha1895 <manha1895@hotmail.com>
2021-08-09 17:02:05 -0700

    edit createBlock

--- components/createBlock.js
+++ components/createBlock.js
@@ -1,19 +1,20 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import styles from "../styles/module_stylesheets/photobanner.module.scss";
 export default function CreateBlock(props) {
-
   switch (props.type) {
     case "2":
-      return(
-        <div className={props.selected === props.value 
-          ? styles.PhotoBanner__block__active
-          : styles.PhotoBanner__block
-        }
-        onClick={(e)=>{
-          e.preventDefault();
-          props.onChange(props.value);
-        }}
-        ref={props.blockref}
+      return (
+        <div
+          className={
+            props.selected === props.value
+              ? styles.PhotoBanner__block__active
+              : styles.PhotoBanner__block
+          }
+          onClick={(e) => {
+            e.preventDefault();
+            props.onChange(props.value);
+          }}
+          ref={props.blockref}
         >
           {props.title}
           <div className={styles.PhotoBanner__block__body}>
@@ -21,64 +22,76 @@ export default function CreateBlock(props) {
             {props.type === "4" && <span>Learn More</span>}
           </div>
         </div>
-      )
+      );
     case "4":
-      return(
-        <div className={props.selected === props.value 
-          ? styles.PhotoBanner__block__active
-          : styles.PhotoBanner__block
-        }
-        onClick={(e)=>{
-          e.preventDefault();
-          props.onChange(props.value);
-        }}
-        ref={props.blockref}
-        >
-          {props.title}
-          <div className={styles.PhotoBanner__block__body}>
-            {props.body}
-            {props.type === "4" && <div className={styles.learnmore}>Learn More</div>}
-          </div>
-        </div>
-      )
-      case "7":
-        return(
-          <div className={props.selected === props.value 
-            ? styles.PhotoBanner__block__type7__active
-            : styles.PhotoBanner__block__type7
+      return (
+        <div
+          className={
+            props.selected === props.value
+              ? styles.PhotoBanner__block__active
+              : styles.PhotoBanner__block
           }
-          onClick={(e)=>{
+          onClick={(e) => {
             e.preventDefault();
             props.onChange(props.value);
           }}
           ref={props.blockref}
-          >
-            {props.title}
-            <div className={styles.PhotoBanner__block__body}>
-              {props.body}
-              {props.type === "7" && <div className={styles.learnmore}>Learn More</div>}
-            </div>
+        >
+          {props.title}
+          <div className={styles.PhotoBanner__block__body}>
+            {props.body}
+            {props.type === "4" && (
+              <div className={styles.learnmore}>Learn More</div>
+            )}
           </div>
-        )
-        case "8":
-          return(
-            <div className={props.selected === props.value 
+        </div>
+      );
+    case "7":
+      return (
+        <div
+          className={
+            props.selected === props.value
               ? styles.PhotoBanner__block__type7__active
               : styles.PhotoBanner__block__type7
-            }
-            onClick={(e)=>{
-              e.preventDefault();
-              props.onChange(props.value);
-            }}
-            ref={props.blockref}
-            >
-              {props.title}
-              <div className={styles.PhotoBanner__block__body}>
-                {props.body}
-                {props.type === "8" && <div className={styles.learnmore}>Learn More</div>}
-              </div>
-            </div>
-          )
+          }
+          onClick={(e) => {
+            e.preventDefault();
+            props.onChange(props.value);
+          }}
+          ref={props.blockref}
+        >
+          <span>{props.title}</span>
+          <div className={styles.PhotoBanner__block__body}>
+            {props.body}
+            {props.type === "7" && (
+              <div className={styles.learnmore}>Learn More</div>
+            )}
+          </div>
+        </div>
+      );
+    case "8":
+      return (
+        <div
+          className={
+            props.selected === props.value
+              ? styles.PhotoBanner__block__type7__active
+              : styles.PhotoBanner__block__type7
+          }
+          onClick={(e) => {
+            e.preventDefault();
+            props.onChange(props.value);
+          }}
+          ref={props.blockref}
+        >
+         <span>{props.title}</span>
+          <div className={styles.PhotoBanner__block__body}>
+            {props.body}
+            {props.type === "8" && (
+              <div className={styles.learnmore}>Learn More</div>
+            )}
+          </div>
+        </div>
+      );
     default:
       break;
   }
