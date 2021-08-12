97295418db3073780c4aafb85e75f010c6ef6c63
manha1895 <manha1895@hotmail.com>
2021-08-12 15:58:31 -0700

    edit blogs styles

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import styles from "../styles/Blogs.module.scss";
+// import styles from "../styles/Blogs.module.scss";
 
 export default function Home() {
   return <div className=""></div>;
