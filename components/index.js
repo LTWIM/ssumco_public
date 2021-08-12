71b107fed04862abf85e6a95bc34497c751bad86
manha1895 <manha1895@hotmail.com>
2021-08-12 16:01:53 -0700

    edit index components

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 export { default as TopBanner } from "./top_banner";
 export { default as Banner } from "./banner";
-export { default as MainBanner } from "./mainBanner";
+export { default as MainBanner } from "./mainbanner";
 export { default as PhotoBanner } from "./photoBanner";
 export { default as VideoBanner } from "./videoBanner";
 export { default as QuoteBanner } from "./quoteBanner";
